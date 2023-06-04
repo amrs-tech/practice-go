6ce51426c4bff5360f9e8e4f61d861fda28a437c
amrs-tech <amrs.tech@gmail.com>
2023-06-05 01:39:53 +0530

    init practice programs

---
+++ prog1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	var newVar string = "sample String"
+	var intVal int = 40
+	fmt.Println("checking var val: ",newVar)
+	fmt.Printf("checking formatted var %v & %v value in some out str\n",newVar, intVal)
+}
